7d0bee64f8ea9d3e5e703dbc311a0e5ebeac9dd3
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-04-02 16:40:42 -0300

    Create ex1.c

---
+++ lista4_5/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+        int a = 14;
+        int b = 3;
+        int c = 18;
+        int d = 22;
+        int soma = (a + b + c + d)/4;
+        printf("A média simples dos valores de a, b, c e d é igual a %i\n", som>
+        return 0;
+}
