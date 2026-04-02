8cf2275a134dbe5597f4d4f6e8c6cb872314e0a3
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-04-02 16:42:44 -0300

    Create ex4.c

---
+++ lista4_5/ex4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+        int a = 0;
+        int b = 0;
+        printf("Digite um número inteiro a: ");
+        scanf("%i", &a);
+        printf("Digite um número inteiro b: ");
+        scanf("%i", &b); 
+        int soma = a + b;
+        if (soma >= 0 && soma < 360){
+                printf("A soma dos ângulos é igual a %i", soma);
+        }else{
+                int diferenca = soma % 360;
+<ando temos %i\n", diferenca);
+        }
+        return 0;
