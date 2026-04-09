e25120978d264f1a01d8125bbd9c7834266658bf
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-04-09 15:38:02 -0300

    Create ex0.c

---
+++ lista6/ex0.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void media(int a, int b){
+        int media = (a + b)/2;
+        printf("A média aritmética é igual a %i\n", media);
+}
+int main(){
+        media(27,23);
+        return 0;
+}
